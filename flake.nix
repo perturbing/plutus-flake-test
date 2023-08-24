ee8769de264180138732eeeda9827f0c1d0eea1b
perturbing <thomas.vellekoop@iohk.io>
2023-08-24 10:33:22 +0200

    correct small typo

--- flake.nix
+++ flake.nix
@@ -43,6 +43,6 @@
     in flake // {
       # Built by `nix build .`
       packages.hello = flake.packages."hello:exe:hello";
-      # packages.world = flake.packages."test:exe:world";
+      # packages.world = flake.packages."world:exe:world";
     });
 }
